000d59eb5344382aa84d7e82551555c5e9630817
Riley Crebs <rcrebs@salesforce.com>
2015-11-14 15:00:21 -0800

    Handle Transformable Objects

--- WSJSONData/WSJSONData.swift
+++ WSJSONData/WSJSONData.swift
@@ -9,39 +9,63 @@
 import Foundation
 
 protocol WSJSONDataProtocol {
-    func keyPathForKey(key: String) -> String
-    func objectForKeyPath(key: String) -> AnyObject?
+    /* Called on object when checking the property name for a key path. This will map an objects property to the JSON key value. If the key and the property name are the same then you can just return the key.
+    @param key The key in the JSON object that needs to be mapped to a property on the object conforming to this protocol
+    @return The object conforming to this protocol must return the property name that should be mapped to the JSON key.  If the key and the property name are the same just return the key.
+    */
+    func propertyNameForKey(key: String) -> String
+    
+    /* The object conforming to the WSJSONDataProtocol is responsible for providing newly allocated objects.  When a new JSON object is encounder the conforming object will be asked for the new object to poulate with JSON
+    @param key The JSON key for the new JSON object. It is the conforming objects job to provide the object to be poplualted by looking at the key and determining what object to return.
+    @param valude The object conforming to the protocol may use the value to determine what object to create or find.
+    @return Object to be poplated in the matching key and value
+    */
+    func objectForKeyPath(key: String, value: AnyObject?) -> AnyObject?
+    
+    /* If a property type is a transformable type the conforming object returns true.
+    @param key The key to check if a property type is transformable
+    return True if the property value/type is suppose to be transformable. False otherwise.
+    */
+    func isTransFormableValueForKeyPath(key: String) -> Bool
 }
 
 class WSJSONData: NSObject {
     func populate(object: AnyObject, jsonData: AnyObject) {
         if NSJSONSerialization.isValidJSONObject(jsonData) {
             for (key, value) in jsonData as! Dictionary<String, AnyObject> {
-                self.populatePropertyFor(object, key: key, value: value)
+                if ((value as? NSNull) == nil) {
+                    self.populatePropertyFor(object, key: key, value: value)
+                }
             }
         }
     }
     
     private func getKeyPathForKey(key: String, object: WSJSONDataProtocol) -> String {
-       return object.keyPathForKey(key)
+       return object.propertyNameForKey(key)
     }
     
-    private func getObjectForKey(key: String, parentObject: WSJSONDataProtocol) -> AnyObject? {
-        return parentObject.objectForKeyPath(key);
+    private func getObjectForKey(key: String, value: AnyObject?, parentObject: WSJSONDataProtocol) -> AnyObject? {
+        return parentObject.objectForKeyPath(key, value: value);
+    }
+    
+    private func isTransformableValueForKeyPath(key:String, object: WSJSONDataProtocol) -> Bool {
+        return object.isTransFormableValueForKeyPath(key)
     }
     
     private func populateSubObjectForParent(parentObject: AnyObject?, subObjectKey: String, subObjectData: Dictionary<String, AnyObject>) {
-        if let objectForKey = self.getObjectForKey(subObjectKey, parentObject: parentObject as! WSJSONDataProtocol) {
+        if let objectForKey = self.getObjectForKey(subObjectKey, value: subObjectData, parentObject: parentObject as! WSJSONDataProtocol) {
             self.populate(objectForKey, jsonData: subObjectData)
         }
     }
     
     private func updatePropertyFor(object: AnyObject, jsonKey: String, jsonValue: AnyObject) {
         if ((object as? WSJSONDataProtocol) != nil) {
+            
             var keyPath: NSString = self.getKeyPathForKey(jsonKey, object: object as! WSJSONDataProtocol)
             keyPath = (keyPath.length > 0) ? keyPath : jsonKey;
             if (object.respondsToSelector(Selector(keyPath as String))) {
-                object.setValue(jsonValue, forKeyPath: keyPath as String)
+                let objectValue = self.transformableValueForKeyPath(jsonKey, value: jsonValue, object: object)
+                object.setValue(objectValue, forKeyPath: keyPath as String)
             }
         }
     }
@@ -52,5 +76,20 @@ class WSJSONData: NSObject {
         } else {
             self.updatePropertyFor(object, jsonKey: key, jsonValue: value)
         }
+    }
+    
+    private func transformableValueForKeyPath(key: String, value: AnyObject, object: AnyObject) -> AnyObject {
+        var objectValue = value;
+        if value as? String != nil {
+            if self.isTransformableValueForKeyPath(key, object: object as! WSJSONDataProtocol) {
+                let objectData: NSData = ((value as? NSString)?.dataUsingEncoding(NSUTF8StringEncoding))!
+                do {
+                    try objectValue = NSJSONSerialization.JSONObjectWithData(objectData, options: NSJSONReadingOptions.AllowFragments)
+                } catch {
+                    objectValue = NSNull()
+                }
+            }
+        }
+        return objectValue
     }
 }
--- WSJSONDataTests/WSJSONDataTests.swift
+++ WSJSONDataTests/WSJSONDataTests.swift
@@ -10,17 +10,6 @@ import XCTest
 @testable import WSJSONData
 
 class WSJSONDataTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testPopulate_WithValidData_ShouldPopulate() {
         let jsonTestData = ["icon_key": "iconvalue", "name_key": "namevalue"]
         let mockObject = TestObject()
@@ -88,27 +77,51 @@ class WSJSONDataTests: XCTestCase {
         XCTAssertEqual(mockSubObject?.name, jsonSubObjectTestData["name"])
         
     }
+    
+    func testPopulate_WithNSNull_ShouldSkip() {
+        let jsonTestData = ["icon": NSNull(), "name_key": "namevalue", "lat": 52]
+        let mockObject = TestObject()
+        let jsonData = WSJSONData()
+        jsonData.populate(mockObject, jsonData: jsonTestData)
+        XCTAssertNil(mockObject.icon)
+        XCTAssertEqual(mockObject.name, jsonTestData["name_key"])
+        XCTAssertEqual(mockObject.lat, jsonTestData["lat"])
+    }
+    
+    func testPopulate_WithTransformable_ShouldHandleTransformable() {
+        let jsonTestData = ["icon": "iconValue", "name_key": "namevalue", "lat": 52, "transformableObject":  "{\"0\":0,\"1\":0,\"2\":0,\"3\":0,\"4\":0,\"5\":0,\"6\":0,\"7\":0,\"8\":0,\"9\":0,\"10\":0,\"11\":0,\"12\":0,\"13\":0,\"14\":0,\"15\":0,\"16\":0,\"17\":0,\"18\":0,\"19\":0,\"20\":0,\"21\":0,\"22\":0,\"23\":0}"]
+        let mockObject = TestObject()
+        let jsonData = WSJSONData()
+        jsonData.populate(mockObject, jsonData: jsonTestData)
+        XCTAssertEqual(mockObject.icon, jsonTestData["icon"])
+        XCTAssertEqual(mockObject.name, jsonTestData["name_key"])
+        XCTAssertEqual(mockObject.lat, jsonTestData["lat"])
+        XCTAssertTrue((mockObject.transformable as? Dictionary<String, Int>) != nil)
+    }
 }
 
 class TestObject: NSObject, WSJSONDataProtocol {
     var icon: String?
     var name: String?
     var lat: NSNumber?
+    var transformable: AnyObject?
+    
     var subObject: TestSubObject?
     var nonWSJSONDataProtocolsubObject: TestSubObjectThatDoesNotConformToWSJSONDataProtocol?
-//    var array: Array?
     
-    func keyPathForKey(key: String) -> String {
+    func propertyNameForKey(key: String) -> String {
         var keyPath = key
         if key == "icon_key" {
             keyPath = "icon"
         } else if key == "name_key" {
             keyPath = "name"
+        } else if key == "transformableObject" {
+            keyPath = "transformable"
         }
         return keyPath
     }
     
-    func objectForKeyPath(key: String) -> AnyObject? {
+    func objectForKeyPath(key: String, value: AnyObject?) -> AnyObject? {
         var object: AnyObject? = nil
         if key == "nonConformSubObject" {
             if self.nonWSJSONDataProtocolsubObject == nil {
@@ -123,6 +136,10 @@ class TestObject: NSObject, WSJSONDataProtocol {
         }
         return object
     }
+    
+    func isTransFormableValueForKeyPath(key: String) -> Bool {
+        return key == "transformableObject"
+    }
 }
 
 class TestSubObjectThatDoesNotConformToWSJSONDataProtocol: NSObject {
@@ -136,11 +153,15 @@ class TestSubObject: NSObject, WSJSONDataProtocol {
     var name: String?
     var address: String?
     
-    func keyPathForKey(key: String) -> String {
+    func propertyNameForKey(key: String) -> String {
         return key
     }
     
-    func objectForKeyPath(key: String) -> AnyObject? {
+    func objectForKeyPath(key: String, value: AnyObject?) -> AnyObject? {
         return nil
+    }
+    
+    func isTransFormableValueForKeyPath(key: String) -> Bool {
+        return false
     }
 }
